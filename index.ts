4465bc3833d76d680a0474573d938b846505c6a8
pufflik <pufflik@gmail.com>
2021-05-19 16:01:22 +0300

    Fix types

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import {IncomingMessage, ServerResponse} from 'http'
 
-export const awsHttpsRedirect = (params: {request: IncomingMessage, response: ServerResponse}) => {
-  if(params.request && params.request.headers['x-forwarded-proto'] !== 'https' && params.response.writeHead) {
+export const awsHttpsRedirect = (params: {
+  request: IncomingMessage | undefined
+  response: ServerResponse | undefined
+}) => {
+  if(params.request && params.request.headers['x-forwarded-proto'] !== 'https' && params.response?.writeHead) {
     params.response.writeHead(301, {Location: `https://${params.request.headers.host}${params.request.url}`}).end()
   }
 }
--- out/index.js
+++ out/index.js
@@ -3,7 +3,8 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 var awsHttpsRedirect = function (params) {
-    if (params.request && params.request.headers['x-forwarded-proto'] !== 'https' && params.response.writeHead) {
+    var _a;
+    if (params.request && params.request.headers['x-forwarded-proto'] !== 'https' && ((_a = params.response) === null || _a === void 0 ? void 0 : _a.writeHead)) {
         params.response.writeHead(301, { Location: "https://" + params.request.headers.host + params.request.url }).end();
     }
 };
